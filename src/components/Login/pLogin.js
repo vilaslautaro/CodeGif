186c8c78da5cad1577562293f61b551ee973103e
Lautaro Vilas <vilaslautaro@gmail.com>
2022-04-24 19:33:03 -0300

    and create with the  ReactDOM.createPortal a modal for login

--- src/components/Login/pLogin.js
+++ src/components/Login/pLogin.js
@@ -7,6 +7,7 @@ import { ErrorMessage } from '@hookform/error-message';
 export default function Register({ onLogin }) {
   const { signInWithEmailAndPassword } = useAuth();
   const [, navigate] = useLocation();
+  
   const {
     handleSubmit,
     register,
